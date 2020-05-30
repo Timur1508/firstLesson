9b362706543bdfdadbd8d00179832a5f09973881
Ivan <foxiti@mailr.ru>
2020-05-30 20:44:31 +0500

    secondLesson

--- script.js
+++ script.js
@@ -16,12 +16,28 @@ let appData = {
 for (let i = 0; i < 2; i++) {
 	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
 	b = prompt("Во сколько обойдется?", '');
+	if ((typeof(a))==='string' && (typeof(a)) != null) && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+			console.log("done");
+			appData.expenses[a] = b;
+	} else {
 
-	appData.expenses[a] = b;
+	}
+	
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежеднеынй бюджет: " + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+	console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay < 2000) {
+	console.log("Высокий уровень достатка");
+} else {
+	console.log("Ошибка");
 }
-
-alert(appData.budget / 30);
-
 //let num = 80;
 
 //if (num < 49) {
